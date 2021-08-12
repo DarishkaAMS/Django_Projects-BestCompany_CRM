159ad681fc4220fa81355c06c3afb09e23173027
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-08-12 06:47:33 +0300

    Add BestCompanyCRM

---
+++ BestCompanyCRM/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for BestCompanyCRM project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BestCompanyCRM.settings')
+
+application = get_wsgi_application()
