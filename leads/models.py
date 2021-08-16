dff13076125b86f5fdc5408327244ad62dfb62fa
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-08-16 06:50:49 +0300

    Add Agent class

--- leads/models.py
+++ leads/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 
 
+class Agent(models.Model):
+    first_name = models.CharField(max_length=25)
+    last_name = models.CharField(max_length=25)
+
+
 class Lead(models.Model):
     SOURCE_CHOICES = (
         ('YouTube', 'YouTube'),
@@ -13,12 +18,14 @@ class Lead(models.Model):
     first_name = models.CharField(max_length=25)
     last_name = models.CharField(max_length=25)
     age = models.IntegerField(default=0)
+    agent = models.ForeignKey(Agent, on_delete=models.CASCADE)
 
     contacted = models.BooleanField(default=False)
     source = models.CharField(choices=SOURCE_CHOICES, max_length=100)
 
     profile_picture = models.ImageField(blank=True, null=True)
     special_files = models.FileField(blank=True, null=True)
+
 
 
 
