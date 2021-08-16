a491fee92531d2be4a548580ea739177a7e03da5
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-08-16 06:36:58 +0300

    Complete Lead class

--- leads/models.py
+++ leads/models.py
@@ -2,7 +2,23 @@ from django.db import models
 
 
 class Lead(models.Model):
+    SOURCE_CHOICES = (
+        ('YouTube', 'YouTube'),
+        ('Facebook', 'Facebook'),
+        ('Instagram', 'Instagram'),
+        ('Google', 'Google'),
+        ('Newsletter', 'Newsletter'),
+    )
+
     first_name = models.CharField(max_length=25)
     last_name = models.CharField(max_length=25)
     age = models.IntegerField(default=0)
+
+    contacted = models.BooleanField(default=False)
+    source = models.CharField(choices=SOURCE_CHOICES, max_length=100)
+
+    profile_picture = models.ImageField(blank=True, null=True)
+    special_files = models.FileField(blank=True, null=True)
+
+
 
